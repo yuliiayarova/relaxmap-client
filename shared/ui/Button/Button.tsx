0fec9ea45e1d2700f2c114203a67c71ea32bc406
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-06-05 23:20:05 +0300

    add base structure

---
+++ shared/ui/Button/Button.tsx
@@ -0,0 +1,37 @@
+import css from './Button.module.css';
+import clsx from 'clsx';
+import Link from 'next/link';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text?: string;
+  className?: string;
+  href?: string;
+  target?: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+export default function Button({
+  text,
+  className,
+  href,
+  target,
+  onClick,
+  children,
+  ...props
+}: ButtonProps) {
+  if (href) {
+    return (
+      <Link href={href} target={target} className={clsx(css.btn, className)}>
+        {text}
+      </Link>
+    );
+  }
+
+  return (
+    <button {...props} className={clsx(css.btn, className)} onClick={onClick}>
+      {text}
+      {children}
+    </button>
+  );
+}
