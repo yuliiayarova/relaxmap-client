0e3ca25dc2ed6e446235bfde4cd0b75c4108092a
shtyktasya <stiktasa@gmail.com>
2026-06-09 15:42:46 +0300

    up footer.tsx and footer.module.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import '@/styles/global.css';
 import { Toaster } from 'react-hot-toast';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 import Header from '@/components/Header/Header';
-
+import { Footer } from '@/components/Footer/Footer';
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
@@ -28,8 +28,11 @@ export default function RootLayout({
         <TanStackProvider>
           <Header />
           {children}
+          
           <Toaster position="bottom-center" />
+          <Footer/>
         </TanStackProvider>
+
       </body>
     </html>
   );
