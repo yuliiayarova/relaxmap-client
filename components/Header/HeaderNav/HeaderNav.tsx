a19e94e123a8384515c483226a95feec1aa50abf
vjosik <108922728+vjosik@users.noreply.github.com>
2026-06-11 22:24:19 +0300

    add route after log out

--- components/Header/HeaderNav/HeaderNav.tsx
+++ components/Header/HeaderNav/HeaderNav.tsx
@@ -1,18 +1,23 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import css from './HeaderNav.module.css';
+import { AuthUser } from '@/lib/api/types/userTypes';
 
 interface HeaderNavProps {
+  user?: AuthUser | null;
   isLoggedIn: boolean;
   className?: string;
   onNavigate?: () => void;
 }
 
 export default function HeaderNav({
+  user,
   isLoggedIn,
   className,
   onNavigate,
 }: HeaderNavProps) {
+
+    const user = user.
   return (
     <nav
       className={clsx(css.navMenu, className)}
