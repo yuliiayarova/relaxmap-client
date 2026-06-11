bc75d967730e285b87b308f9215a3dcf00f25689
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-06-11 12:29:49 +0300

    fix redirect profile in register, login forms

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'ftp.goit.study',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ac.goit.global',
       },
     ],
   },
